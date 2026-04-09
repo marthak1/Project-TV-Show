c0ef3e9290d85c720c778255508313e6c5e8953e
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-09 22:47:28 +0100

    create innerHtml elements to contain episode data

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const sectionEl= document.createElement("section");
+  rootElem.appendChild(sectionEl)
+  const h1El = document.createElement("h1")
+  sectionEl.appendChild(h1El)
+  h1El.textContent = textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
+
