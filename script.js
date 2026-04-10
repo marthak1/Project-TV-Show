ac8b0cbdb9999860113a277b360d8596d9651d31
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-10 19:59:26 +0100

    renamed DOM display function and created element tags to display a single episode

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const oneEpisode = getOneEpisode()
+  renderEpisodes(oneEpisode);
 }
 
-function makePageForEpisodes(episodeList) {
+function renderEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  const sectionEl= document.createElement("section");
-  rootElem.appendChild(sectionEl)
-  const h1El = document.createElement("h1")
-  sectionEl.appendChild(h1El)
-  h1El.textContent = textContent = `Got ${episodeList.length} episode(s)`;
+  const sectionEl = document.createElement("section");
+  const innerDivEl = document.createElement("div");
+  const h1El = document.createElement("h1");
+  const imageEl = document.createElement("img");
+  const pElemRuntime = document.createElement("p")
+  const pElemSummary = document.createElement("p");
+  
+  rootElem.appendChild(sectionEl);
+  sectionEl.appendChild(innerDivEl);
+  innerDivEl.appendChild(h1El);
+  innerDivEl.appendChild(imageEl);
+  innerDivEl.appendChild(pElemRuntime);
+  innerDivEl.appendChild(pElemSummary);
+
+  h1El.textContent = episodeList.name;
+  imageEl.src = episodeList.image.medium;
+  pElemRuntime.textContent = episodeList.runtime;
+  pElemSummary.textContent = episodeList.summary;
 }
 
 window.onload = setup;
+
 
