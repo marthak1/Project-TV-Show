9758aaf90c281838f9b74569b5b7fc03b8d0bf5d
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-19 23:43:55 +0100

    add searcbar feature and completed level 300 work

--- script.js
+++ script.js
@@ -2,12 +2,30 @@
 let state = {
   episodes: [],
   searchTerm: "",
+  counterEl: null,
+};
+let cachedEpisodeData = null;
+const getAllEpisodes = async (url) => {
+  if (cachedEpisodeData) return cachedEpisodeData;
+  try {  
+    const response = await fetch(url);
+    if(!response.ok){
+    throw new Error(`HTTP: ${response.status}`);
+    }
+    cachedEpisodeData = await response.json();
+    return cachedEpisodeData;
+  } catch (error) {
+    console.error("Failed to load episodes",error)
+    alert("Failed to load episodes");
+  } 
 };
 
 //Responsibility => Should orchestrate All layers
-function setup() {
-  //Fetch raw episode data
-  const allEpisodes = getAllEpisodes();
+async function setup() {
+  //Fetch raw episode data from API
+  const allEpisodes = await getAllEpisodes(
+    "https://api.tvmaze.com/shows/82/episodes",
+  );
 
   //Prepared Episode Data => Combines formatted property and every other property the UI needs
   const preparedEpisodeData = allEpisodes.map((episode) => {
@@ -16,22 +34,26 @@ function setup() {
     return {
       name: episode.name,
       code: formatEpisodeCode(season, number),
-      image: episode.image.medium,
+      image: episode.image?.medium || "",
       runtime: formatRuntime(runtime),
       summary: episode.summary,
     };
   });
 
-  state.episodes = preparedEpisodeData;
+  state.episodes = preparedEpisodeData;  //<--------- Add prepared data for state management
   state.searchTerm = "";
 
   renderApp(state.episodes);
+  hideLoadingOverlay();
+  
 }
 
-//Render the full application structure once
+//Responsibility => Renders page => Render the full application structure once 
 function renderApp(episodeList) {
+  const bodyEl = document.querySelector("body");
   const rootElem = document.getElementById("root");
-  rootElem.innerHTML = "";
+  const oldCards = rootElem.querySelectorAll(".episode-card");
+    oldCards.forEach((card) => card.remove());
 
   const headerSectionEl = document.createElement("section");
   headerSectionEl.classList.add("header-section");
@@ -42,32 +64,63 @@ function renderApp(episodeList) {
   const title = document.createElement("h2");
   title.textContent = "Game of Thrones TV Episodes";
   navBarEl.appendChild(title);
-  
+
   const searchBarEl = searchInput(state.episodes, renderEpisodeList);
   searchBarEl.classList.add("search-bar");
   navBarEl.appendChild(searchBarEl);
   rootElem.appendChild(headerSectionEl);
+
+
+  const loadingOverlayEl = document.createElement("div");
+  loadingOverlayEl.classList.add("loading-overlay");
+  loadingOverlayEl.id = "loadingOverlay";
+  bodyEl.appendChild(loadingOverlayEl);
+  const spinnerEl = document.createElement("div");
+  spinnerEl.classList.add("spinner");
+  loadingOverlayEl.appendChild(spinnerEl);
 
   const sectionEl = document.createElement("section");
   sectionEl.classList.add("episode-section");
   sectionEl.id = "episode-section";
   rootElem.appendChild(sectionEl);
 
-  renderEpisodeList(episodeList);
+  const counter = document.createElement("p");
+  counter.classList.add("counter");
+  navBarEl.appendChild(counter);
+  state.counterEl = counter;
+
+  // credit
+  const footerEl = document.createElement("footer")
+  const credit = document.createElement("p");
+  footerEl.appendChild(credit)
+  credit.id = "credit";
+  credit.innerHTML = `
+    Data originally from 
+    <a href="https://tvmaze.com/" target="_blank">TVMaze.com</a>
+  `;
+  rootElem.appendChild(footerEl);
+  renderEpisodeList(state.episodes);
+  
 }
 
+//Responsibilities renders episode list
 function renderEpisodeList(episodeList) {
+  const rootElem = document.getElementById("root");
   const sectionEl = document.getElementById("episode-section");
   if (!sectionEl) {
     return;
   }
 
-  sectionEl.innerHTML = "";
+  // sectionEl.innerHTML = "";
+  const oldCards = rootElem.querySelectorAll(".episode-card");
+  oldCards.forEach((card) => card.remove());
   for (const episode of episodeList) {
     const episodeCard = createEpisodeCard(episode);
     sectionEl.appendChild(episodeCard);
   }
+  updateCounter(episodeList, state.episodes);
 }
+
 //UI Component Card => Responsibility => Should take one episode data, create DOM Elements and return a fully built episode card
 function createEpisodeCard(episode) {
   const articleEl = document.createElement("article");
@@ -105,6 +158,7 @@ function createEpisodeCard(episode) {
   return articleEl;
 }
 
+//Responsibility => Creates a search input element, stores value, listens for event triggers and calls filter function
 function searchInput(episodes, renderfn) {
   const searchBoxEl = document.createElement("input");
   searchBoxEl.type = "search";
@@ -119,6 +173,7 @@ function searchInput(episodes, renderfn) {
   return searchBoxEl;
 }
 
+//Function Responsibility => Filters episode given a list and a query 
 function handleSearchInput(query, episodeList) {
   const searchTerm = query.trim().toLowerCase();
   if (searchTerm === "") {
@@ -130,6 +185,10 @@ function handleSearchInput(query, episodeList) {
     const summary = episode.summary ? episode.summary.toLowerCase() : "";
     return name.includes(searchTerm) || summary.includes(searchTerm);
   });
+}
+
+function updateCounter(filteredEpisodes, allEpisodes) {
+  state.counterEl.textContent = `Showing ${filteredEpisodes.length} of ${allEpisodes.length} episodes`;
 }
 //Formatters => Responsibilities => Should transform data into UI-friendly data
 //transforms season + number properties into format as S01E01
@@ -143,4 +202,27 @@ function formatRuntime(time) {
   const remainingMinute = time % 60;
   return `${String(hour).padStart(2, "0")}:${String(remainingMinute).padStart(2, "0")}`;
 }
+
+function hideLoadingOverlay() {
+  const loadingOverlay = document.getElementById("loadingOverlay");
+  const content = document.querySelector("#root");
+
+  if (!loadingOverlay) return;
+
+  loadingOverlay.style.opacity = "0";
+  loadingOverlay.style.transition = "opacity 0.5s ease";
+
+  setTimeout(() => {
+    loadingOverlay.style.display = "none";
+    content.style.display = "block";
+  },500);
+}
 window.onload = setup;
+
+
+
+
+
+
+
+
